89343cb3aa8aeac8c426cc595f6b73df0bfe704b
l2t4all-boop <l2t4all@gmail.com>
2026-01-24 06:15:39 +0530

    Update EvenOrOddNumber.java
    Added logic even or odd

--- src/main/java/com/careerit/cj/day2/EvenOrOddNumber.java
+++ src/main/java/com/careerit/cj/day2/EvenOrOddNumber.java
@@ -1,4 +1,16 @@
 package com.careerit.cj.day2;
 
 public class EvenOrOddNumber {
+
+        public static void main(String[] args){
+
+              int num = 10;
+
+              if(num % 2 == 0){
+                System.out.println("Even");
+              }else{
+                System.out.println("Odd");
+              }
+
+        }
 }
